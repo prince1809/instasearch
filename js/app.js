6c7d0b3c43b79dfc5b5c4b045e9a31601e5a3272
Prince Kumar <prince18pat@gmail.com>
2015-06-18 12:46:57 +0900

    JS File Add

---
+++ js/app.js
@@ -0,0 +1,17 @@
+    
+    var Search = React.createClass({
+        
+        render: function() {
+            return (
+            <div>I am a returning data</div>
+        );
+    }
+});
+
+
+
+
+React.render(
+  <Search />,
+  document.getElementById('search')
+);
